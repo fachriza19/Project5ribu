eaf77d2a796831dd1715b49232953be408de7f58
faqihf88 <faqihf88@gmail.com>
2018-10-12 08:23:51 +0700

    edit bengkel dan pengamatan laut

--- application/views/pages/sarpras/pengamatan.php
+++ application/views/pages/sarpras/pengamatan.php
@@ -52,6 +52,257 @@
               <h6>Pengolahan dan Pengumpulan Data </h6>
               <h6>Hasil pengolahan data sebagaimana dimaksud diproses  dalam bentuk lembar lukis dan hasil laporan survei setiap kegiatan dilaporkan kepada Kepala Distrik Navigasi untuk selanjutnya disampaikan kepada Direktur Kenavigasian untuk bahan penetapan kebijakan selanjutnya.</h6><br>
               <h6>Kegiatan pengamatan laut dapat dilaksanakan untuk kepentingan pihak ketiga setelah mendapat izin dari Direktur Kenavigasian.</h6>
+
+              <h4>Alat Pengamatan Laut</h4>
+            <table class="table table-striped table-bordered" style="width:100%">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama Peralatan</th>
+                  <th>Type</th>
+                  <th>Merk</th>
+                  <th>Daya</th>
+                  <th>Tahun Perolehan</th>
+                  <th>Produksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>MULTIBEAM ECHOSOUNDER</td>
+                  <td>Geowath</td>
+                  <td>GeoAcoustic</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>MOTION SENSOR</td>
+                  <td>IMU 108</td>
+                  <td>SMC</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>SURFACE SOUND VELOCITY</td>
+                  <td>Micro-X mini SV</td>
+                  <td>AML</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>SIDE SCAN SONAR</td>
+                  <td>CM-2</td>
+                  <td>C-MAX</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>SUB BOTTOM PROFILE</td>
+                  <td>G-822SX</td>
+                  <td>Geometricss</td>
+                  <td>110-230 VAC</td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>MAGNETO METER</td>
+                  <td>G-822SX</td>
+                  <td>Geometricss</td>
+                  <td>110-230 VAC</td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>7</td>
+                  <td>ADL (SOFTWARE & DONGLE)</td>
+                  <td>Hypack</td>
+                  <td>Hypack</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>8</td>
+                  <td>SINGLE BEAM ECHOSOUNDER SYSTEM (DUAL FREQUENCY)</td>
+                  <td>DF3200 – MK-III</td>
+                  <td>Teledyne Odom Echotrack</td>
+                  <td>110-230 VAC / 24 VDC</td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>  
+                  <td>9</td>
+                  <td>SINGLE BEAM ECHOSOUNDER & GPS SYSTEM</td>
+                  <td>CEESPOE-100</td>
+                  <td>CEE Hydrosystem</td>
+                  <td>Battery</td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>10</td>
+                  <td>SOUND VELOCITY & CTD</td>
+                  <td>Minos-X</td>
+                  <td>AML</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>11</td>
+                  <td>CURRENT METER</td>
+                  <td>MODEL-308</td>
+                  <td>VALEPORT</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>12</td>
+                  <td>DIGITAL TIDE GAUGE</td>
+                  <td>Tide Master</td>
+                  <td>VALEPORT</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>13</td>
+                  <td>GNSS RTK ROVER WITH HEADING</td>
+                  <td>SPS-855/SPS-555</td>
+                  <td>Trimble</td>
+                  <td>12 V-DC</td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>14</td>
+                  <td>GNSS RTK BASE</td>
+                  <td>SPS-855</td>
+                  <td>Trimble</td>
+                  <td>12 V-DC</td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>15</td>
+                  <td>RADIO TELEMETRI</td>
+                  <td>TDL-450H</td>
+                  <td>Trimble</td>
+                  <td>12 V-DC</td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>16</td>
+                  <td>TOTAL STATION</td>
+                  <td>M-3</td>
+                  <td>Trimble</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>17</td>
+                  <td>THOUGHT BOOK</td>
+                  <td>CF-53</td>
+                  <td>PANASONIC</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>18</td>
+                  <td>HANDHELD GPS</td>
+                  <td>JUNO 3D</td>
+                  <td>Trimble</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>19</td>
+                  <td>WEATHER STATION</td>
+                  <td>PB-2000</td>
+                  <td>AIRMAR</td>
+                  <td></td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>20</td>
+                  <td>GENERATOR</td>
+                  <td>KW26-852</td>
+                  <td>KRISBOW</td>
+                  <td>5500 Watt</td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>21</td>
+                  <td>PLOTTER (A0)</td>
+                  <td>Designjet T-1300</td>
+                  <td>HP</td>
+                  <td>LISTRIK</td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>22</td>
+                  <td>SCANNER (A0)</td>
+                  <td>Designjet T-1300</td>
+                  <td>HP</td>
+                  <td>LISTRIK</td>
+                  <td>2013</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>23</td>
+                  <td>GPS (MAP) SOUNDER</td>
+                  <td>420s</td>
+                  <td>Garmin</td>
+                  <td>12 Volt DC</td>
+                  <td>2008</td>
+                  <td>Taiwan</td>
+                </tr>
+                <tr>
+                  <td>24</td>
+                  <td>ECHOSOUNDER & GPS</td>
+                  <td>Hydrographic Survey</td>
+                  <td>Ceeducer Pro</td>
+                  <td>12 Volt DC</td>
+                  <td>2010</td>
+                  <td>Australia</td>
+                </tr>
+                <tr>
+                  <td>25</td>
+                  <td>HANDHELD GPS</td>
+                  <td>GPSmap 76csx</td>
+                  <td>Garmin</td>
+                  <td>Battery</td>
+                  <td>2008</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>26</td>
+                  <td>GPS MAP 421S</td>
+                  <td>421S</td>
+                  <td>Garmin</td>
+                  <td>12 Volt DC</td>
+                  <td>2009</td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
