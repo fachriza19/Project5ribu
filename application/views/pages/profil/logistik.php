cb477fe3b1675a8c3b94f4f99a6451dadfd8dc06
Nata <adityapranatama97@gmail.com>
2018-12-04 14:08:02 +0700

    revisian
    revisian

---
+++ application/views/pages/profil/logistik.php
@@ -0,0 +1,62 @@
+<div class="page-wrapper">
+  <div class="container-fluid">
+
+
+
+    <div class="row page-titles">
+      <div class="col-md-5 col-8 align-self-center">
+        <h3 class="text-themecolor">Logistik</h3>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+          <li class="breadcrumb-item active">Logistik</li>
+        </ol>
+      </div>
+    </div>
+
+
+
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <h3 class="card-title">Logistik</h3>
+            <h5 class="text-justify">Sesuai Peraturan Menteri Perhubungan<br> NOMOR: KM 30 TAHUN 2006 Tentang Organisasi Dan Tata Kerja Distrik Navigasi <br><br><center>BAB II<br><br>Tentang Susunan Organisasi<br><br>Pasal 14</center></h5><br>
+
+            <h5 class="text-justify">Bidang Logistik mempunyai tugas melaksanakan penyusunan rencana kebutuhan dan pengadaan, penyimpnan, penyaluran, dan penghapusan perlengkapan dan peralatan untuk penyelenggaraan sarana bantu navigasi pelayaran, fasilitas pangkalan, bengkel, pengamatan laut, survei hidrografi, serta pemantauan alur dan perlintasan.</h5><br>
+
+            <h5>
+              <center>Pasal 15</center><br>
+
+              Dalam melaksanakan tugas sebagaimana dimaksud dalam Pasal 14, Bagian Logistik menyelenggarakan fungsi:
+
+              <ol class="list-icon" type="a">
+                <li>Penyusunan rencana kebutuhan, pengadaan, penyimpanan dan penyaluran perlengkapan dan peralatan sarana bantu navigasi pelayaran, telekomunikasi pelayaran, pengamatan laut, kegiatan hidrografi, alur dan perlintasan kapan negara kenavigasian fasilitas pangkalan dan bengkel.</li>
+                <li>Penyusunan inventarisasi dan penyiapan penghapusan sarana bantu navigasi pelayaran, telekomunikasi pelayran, kapal negara kenavigasian, pengamatan laut, fasilitas pangkalan dan bengkel.</li>
+              </ol>
+            </h5><br>
+
+            <h5>
+              <center>Pasal 16</center><br>
+
+              Bagian Logistik terdiri dari:
+
+              <ol class="list-icon" type="a">
+                <li>Seksi Pengadaan.</li>
+                <li>Seksi Inventarisasi dan penghapusan.</li>
+              </ol>
+            </h5><br>
+
+            <h5>
+              <center>Pasal 17</center><br>
+              <ol>
+                <li>Seksi Pengadaan mempunyai tugas melakukan penyusunan rencana kebutuhan, pengadaan, penyimpanan dan penyaluran perlengkapan dan peralatan sarana bantu navigasi pelayran, telekomunikasi pelayaran, pengamatan laut, kegiatan hidrografi, alur dan perlintasan, kapal negara kenavigasian, fasilitas pangkalan dan bengkel.</li>
+                <li>Seksi Inventarisasi dan Penghapusan mempunyai tugas melakukan penyusunan inventarisasi dan penyiapan penghapusan sarana bantu navigasi pelayaran, telekomunikasi pelayaran, kapal negara kenavigasian, pengamatan laut, fasilitas pangkalan dan bengkel.</li>
+              </ol>
+            </h5>
+        </div>
+      </div>
+    </div>
+ </div>
+
+</div>
+</div>
--- application/views/pages/profil/struktur.php
+++ application/views/pages/profil/struktur.php
@@ -19,19 +19,54 @@
       <div class="col-12">
         <div class="card">
           <div class="card-body">
-<<<<<<< HEAD
-            <img src="<?php echo base_url('assets/images/struktur-org.png'); ?>" width="101%" height="100%">
-          </div>
-=======
-            <img src="<?php echo base_url('assets/images/struktur-org.png'); ?>" width="101%" height="100%">    
->>>>>>> upstream/master
+              <h4 class="card-title">STRUKTUR ORGANISASI</h4><br>
+            <center><img src="<?php echo base_url('assets/images/SO.png'); ?>" width="100%"></center><br><br>
+
+            <h5 class="text-justify">Distrik Navigasi Kelas I Tanjung Priok merupakan Distrik Navigasi Kelas I yang mempunyai Susunan Organisasi sebagai berikut :</h5>
+            <ol class="list-icons" type="1">
+              <h5><li>Kepala Distrik Navigasi</li></h5>
+              <h5 class="text-justify">Kepala Kantor melaksanakan Pembinaan Teknis Administratif dan Teknis Operasional terhadap bagian/ bidang dan kelompok jabatan fungsional, instalasi yang ada dibawahnya serta melaksanakan koordinasi dan atau kerjasama dengan instansi terkait. Kepala Kantor dalam melaksanakan tugas Kepala kantor menyelenggarakan fungsi :</h5>
+              <ol class="list-icons" type="a">
+                <h5 class="text-justify"><li>Penyusunan Rencana dan Program Pengoperasian serta Pengawasan Sarana Bantu Navigasi Pelayaran, Kapal Negara Kenavigasian, Fasilitas Pangkalan, Bengkel, Pengamatan Laut dan Survey Hidrografi serta Pemantauan Alur dan Perlintasan.</li></h5>
+                <h5 class="text-justify"><li> Penyusunan Rencana kebutuhan dan pelaksanaan pengadaan, penyimpanan, penyaluran dan penghapusan perlengkapan dan peralatan untuk Sarana Bantu Navigasi Pelayaran, Kapal Negara Kenavigasian, Fasilitas Pangkalan, Bengkel, Pengamatan Laut dan Survey Hidrografi serta Pemantauan Alur dan Perlintasan.</li></h5>
+                <h5 class="text-justify"><li>Pelaksanaan Program Pengoperasian dan Pemeliharaan Sarana Bantu Navigasi Pelayaran, Kapal Negara Kenavigasian dan  Fasilitas Pangkalan serta Bengkel.</li></h5>
+                <h5 class="text-justify"><li>Pelaksanaan Pengamatan Laut dan Survey Hidrografi serta Pemantauan Alur dan Perlintasan.</li></h5>
+                <h5 class="text-justify"><li>Pelaksanaan Urusan Logistik.</li></h5>
+                <h5 class="text-justify"><li>Pelaksanaan analisis dan evaluasi pengoperasian, pengawakan dan pemeliharaan Sarana Bantu Navigasi Pelayaran, Kapal Negara Kenavigasian, Fasilitas Pangkalan, Bengkel, Pengamatan Laut dan Survey Hidrografi serta Pemantauan Alur dan Perlintasan. </li></h5>
+                <h5 class="text-justify"><li>Pelaksanaan Urusan Keuangan, Kepegawaian, Ketatausahaan, Kerumahtanggaan, Hubungan Masyarakat, Pengumpulan dan Pengolahan Data, Dokumentasi serta Penyusunan Laporan.</li></h5>
+              </ol>
+
+              <h5><li>Bagian Tata Usaha</h5></li>
+              <h5 class="text-justify">Melaksanakan tugas pelaksanaan urusan keuangan, pelaksanaan urusan kepegawaian, ketatausahaan, kerumahtanggaan, hubungan masyarakat, pengumpulan data dan pengolahan data, dokumentasi serta penyusunan laporan. Bagian Tata Usaha terdiri dari :</h5>
+              <ol class="list-icons" type="a">
+                <h5><li>Subbagian Keuangan</h5></li>
+                <h5 class="text-justify">Mempunyai tugas melakukan urusan keuangan, meliputi penyusunan kebutuhan anggaran, perbendaharaan, verifikasi dan pelaporan keuangan.</h5>
+                <h5><li>Subbagian Kepegawaian dan Umum</h5></li>
+                <h5 class="text-justify">Mempunyai tugas melakukan urusan kepegawaian, ketatatausahaan, kerumahtanggaan, hubungan masyarakat, pengumpulan dan pengolahan data, dokumentasi serta penyusunan laporan.</h5>
+              </ol>
+
+              <h5><li>Bidang Operasi</h5></li>
+              <h5 class="text-justify">Melaksanakan tugas penyusunan rencana, program, pengoperasian, pemeliharaan, pengawasan, analisa, evaluasi dan penyusunan laporan sarana bantu navigasi pelayaran, telekomunikasi pelayaran, kapal Negara Kenavigasian, fasilitas pangkalan, bengkel, pengamatan laut, survey hidrografi serta pemantauan alur dan pelintasan. Bidang Operasi terdiri dari :</h5>
+              <ol class="list-icons" type="a">
+                <h5><li>Seksi Program dan Evaluasi</h5></li>
+                <h5 class="text-justify">Mempunyai tugas melakukan penyusunan rencana dan program, analisis dan evaluasi serta penyusunan laporan kegiatan penyelenggaraan sarana bantu navigasi pelayaran, telekomunikasi pelayaran, kapal Negara Kenavigasian, fasilitas pangkalan, bengkel, pengamatan laut, survey hidrografi serta pemantauan alur dan pelintasan.</h5>
+                <h5><li>Seksi Operasi Sarana dan Prasarana</h5></li>
+                <h5 class="text-justify">Mempunyai tugas melakukan penggoperasian, pemeliharaan, pengawasan kegiatan penyelenggaraan sarana bantu navigasi pelayaran, telekomunikasi pelayaran, kapal Negara Kenavigasian, fasilitas pangkalan, bengkel, pengamatan laut, survey hidrografi serta pemantauan alur dan pelintasan.</h5>
+              </ol>
+
+              <h5><li>Bidang Logistik</h5></li>
+              <h5 class="text-justify">Melaksanakan tugas penyusunan rencana kebutuhan dan pengadaan, penyimpanan, penyaluran dan penghapusan perlengkapan dan peralatan untuk penyelenggaraan sarana bantu navigasi pelayaran, telekomunikasi pelayaran, kapal Negara Kenavigasian, fasilitas pangkalan, bengkel, pengamatan laut, survey hidrografi serta pemantauan alur dan pelintasan. Bidang Logistik terdiri dari :</h5>
+              <ol class="list-icons" type="a">
+                <h5><li>Seksi Pengadaan</h5></li>
+                <h5 class="text-justify">Mempunyai tugas melakukan penyusunan rencana kebutuhan, pengadaan, penyimpanan dan penyaluran perlengkapan dan peralatan sarana bantu navigasi pelayaran, telekomunikasi pelayaran, kapal Negara Kenavigasian, fasilitas pangkalan, bengkel, pengamatan laut, survey hidrografi serta pemantauan alur dan pelintasan.</h5>
+                <h5><li>Seksi Inventarisasi dan Penghapusan</h5></li>
+                <h5 class="text-justify">Mempunyai tugas melakukan penyusunan inventarisasi dan penyiapan penghapusan  sarana bantu navigasi pelayaran, telekomunikasi pelayaran, kapal Negara Kenavigasian, fasilitas pangkalan dan bengkel.</h5>
+              </ol>
+            </ol>
         </div>
       </div>
     </div>
+ </div>
 
-
-
-  </div>
 </div>
-
 </div>
